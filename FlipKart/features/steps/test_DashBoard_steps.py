868592f7aed40775468ba4f8d014f3b5cbe479f5
mpavanka <pavankalyan68335@gmail.com>
2025-09-07 12:30:59 +0530

    made some changes

--- FlipKart/features/steps/test_DashBoard_steps.py
+++ FlipKart/features/steps/test_DashBoard_steps.py
@@ -1,15 +1,15 @@
-from behave import Given, When, Then
+from behave import given, when, then
 from behave import *
 from playwright.sync_api import sync_playwright
     
-@Given("User is on FlipKart login page")
+@given("User is on FlipKart homepage")
 def user_is_on_FlipKart_login_page(self):
     print("User is on FlipKart login page")
 
-@When("User searches for {string}")
+@when("User searches for {string}")
 def user_searches_for(self, product):
     pass
 
-@Then("User should see the search results page with relevant products")
+@then("User should see the search results page with relevant products")
 def user_should_see_the_search_results_page_with_relevant_products(self):
     pass
